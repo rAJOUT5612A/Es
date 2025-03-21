e7a379d188802ef424b0bc2e525423df6014b507
rAJOUT5612A <lena12q@outlook.com>
2025-03-21 20:04:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8107300017:AAELygetn24lzyNDq5huQ-HsJOssFcPIykw")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAJPUTDDOS")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1329951770")
+
+
+
+
+
+
+
+  
